8f1057c0431a5fc86e16faef3f4e6b2a119d7f4b
Nicholas DiPatri <ndipatri@gmail.com>
2024-10-06 19:10:55 -0400

    image updates from Mike!

--- composeApp/src/commonMain/kotlin/screens/SleepScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/SleepScreen.kt
@@ -1,18 +1,45 @@
 package screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import content.ScreenContent
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 import robogaggiamultiplatform.composeapp.generated.resources.*
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun SleepScreen(onWakeClicked: () -> Unit) {
     ScreenContent(
-        body1Resource = Res.string.i_am_sleeping,
-        body2Resource = Res.string.clear_scale,
         button1Resource = Res.string.wake,
         onFirstButtonClick = onWakeClicked,
         backgroundImage = Res.drawable.night
-    )
+    ) {
+        Column {
+            Row {
+                Text(
+                    modifier = Modifier.padding(top = 50.dp),
+                    textAlign = TextAlign.Left,
+                    text = stringResource(Res.string.clear_scale)
+                )
+            }
+        }
+    }
 }
--- composeApp/src/commonMain/kotlin/screens/WelcomeScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/WelcomeScreen.kt
@@ -6,16 +6,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
-import robogaggiamultiplatform.composeapp.generated.resources.*
+import robogaggiamultiplatform.composeapp.generated.resources.Res
+import robogaggiamultiplatform.composeapp.generated.resources.coffee_robot
+import robogaggiamultiplatform.composeapp.generated.resources.welcome_primary
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
@@ -26,13 +30,17 @@ fun WelcomeScreen() {
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row {
+        Row (verticalAlignment = Alignment.CenterVertically) {
             Image(
                 modifier = Modifier.weight(.2f),
-                painter = painterResource(Res.drawable.coffee_face),
+                painter = painterResource(Res.drawable.coffee_robot),
                 contentDescription = null
             )
-            Text(text = stringResource(Res.string.welcome_primary))
+            Text(
+                modifier = Modifier.width(460.dp).padding(horizontal = 30.dp),
+                textAlign = TextAlign.Center,
+                text = stringResource(Res.string.welcome_primary)
+            )
         }
     }
 }
